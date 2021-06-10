157c4a3d155d9b0a67ef8d7ac20d88c1199553d8
warrior-coder <gudkou2003@gmail.com>
2021-06-10 10:03:47 +0300

    BEM methodology

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // -+-+-+-+-+-+-+-+-+-+- Creation part -+-+-+-+-+-+-+-+-+-+-
-const BOTTOM = document.querySelector('.bottom_container');
-const BOTTOMS = document.querySelectorAll('.circle-bottom');
-const TOP = document.querySelector('.top_container');
-const GAMEOVER = document.querySelector('.gameover_container');
+const BOTTOM = document.querySelector('.electrician__game_bottom');
+const BOTTOMS = document.querySelectorAll('.electrician__circle-bottom');
+const TOP = document.querySelector('.electrician__game_top');
+const WIRES = document.querySelectorAll('.electrician__wire');
+const GAMEOVER = document.querySelector('.electrician_gameover');
 
-let tops_position = [ 0, 1, 2, 3, 4 ];
-let bottoms_position = [ 0, 1, 2, 3, 4 ];
+var tops_position = [ 0, 1, 2, 3, 4 ];
+var bottoms_position = [ 0, 1, 2, 3, 4 ];
 
+// Mix top circles
 for (let i = 0; i < 5; i++)
 {
     let r = Math.round(Math.random() * 4);
@@ -14,27 +16,14 @@ for (let i = 0; i < 5; i++)
     tops_position[i] = tops_position[r];
     tops_position[r] = temp;
 }
-for (let i = 0; i < 5; i++)
-{
-    TOP.innerHTML += '<img class="circle-top" src="images/circle-top-' + tops_position[i] + '.svg" alt="circle" draggable="false">'
-}
+
+// Attach top circles
+for (let i = 0; i < 5; i++) TOP.innerHTML += '<img class="electrician__circle-top" src="images/circle-top-' + tops_position[i] + '.svg" alt="circle" draggable="false">'
+const TOPS = document.querySelectorAll('.electrician__circle-top');
+
 
 // -+-+-+-+-+-+-+-+-+-+- Logic part -+-+-+-+-+-+-+-+-+-+-
-var TARGET = -1,
-    mX0, mY0,
-    dX, dY,
-    OK = 0,
-    DONE = 0;
-const LINES = document.querySelectorAll('.line');
-const TOPS = document.querySelectorAll('.circle-top');
-
-function GameOver(mode)
-{
-    TOP.style.opacity = BOTTOM.style.opacity = .5;
-    GAMEOVER.style.zIndex = 99;
-    GAMEOVER.style.opacity = 1;
-    GAMEOVER.innerHTML = '<img style="height: 34vh;" src="images/' + mode + '.svg" alt="gameover" draggable="false">';
-}
+var TARGET = -1, mX0, mY0, dX, dY, OK = 0, DONE = 0;
 
 for (let i = 0; i < 5; i++)
 {
@@ -49,7 +38,6 @@ for (let i = 0; i < 5; i++)
             dX = dY = 0;
         }
     });
-
     TOPS[i].addEventListener('mouseup', () => 
     {
         if (TARGET > -1 && tops_position[i] > -1)
@@ -58,8 +46,6 @@ for (let i = 0; i < 5; i++)
             DONE++;
             if (tops_position[i] == TARGET) OK++;
             else GameOver('lose');
-
-            // If all done
             if (OK == 5) GameOver('win');
 
             // Lock circles
@@ -81,13 +67,13 @@ document.addEventListener('mousemove', e =>
 
         // Count length
         let len = Math.round(Math.sqrt(dX*dX + dY*dY));
-        LINES[TARGET].style.height = len + 'px';
+        WIRES[TARGET].style.height = len + 'px';
 
         // Count rotating
         let deg = -Math.atan(dX/dY) / 3.14 * 180;
         if (dY > 0) deg += 180;
-        LINES[TARGET].style.transform = BOTTOMS[TARGET].style.transform = 'rotate(' + deg + 'deg)';
-        LINES[TARGET].style.display = 'block';
+        WIRES[TARGET].style.transform = BOTTOMS[TARGET].style.transform = 'rotate(' + deg + 'deg)';
+        WIRES[TARGET].style.display = 'block';
     }
 });
 
@@ -97,7 +83,15 @@ document.addEventListener('mouseup', () =>
     if (TARGET > -1)
     {
         BOTTOMS[TARGET].style.transform = 'rotate(0deg)';
-        LINES[TARGET].style.display = 'none';
+        WIRES[TARGET].style.display = 'none';
     }
     TARGET = -1;
 });
+
+function GameOver(mode)
+{
+    TOP.style.opacity = BOTTOM.style.opacity = .5;
+    GAMEOVER.style.zIndex = 99;
+    GAMEOVER.style.opacity = 1;
+    GAMEOVER.innerHTML = '<img style="height: 34vh;" src="images/' + mode + '.svg" alt="gameover" draggable="false">';
+}
